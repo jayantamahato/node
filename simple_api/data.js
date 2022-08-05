01162cc521e52a67ca0c406bf5439ad05a91f68b
jayanta <jayantamahato830@gmail.com>
2022-08-05 23:39:29 +0530

    static api

---
+++ simple_api/data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:'jayanta',email:'jayanta@test.com'},
+    {name:'Bappa',email:'bappa@test.com'},
+    {name:'Dip',email:'dip@test.com'},
+    {name:'Sankha',email:'sankha@test.com'}
+]
+module.exports=data
